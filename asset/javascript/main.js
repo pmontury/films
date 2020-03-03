e10351cc56ea270e4f59e91236b86c24abe12589
youngrecursive <baz.martin42@gmail.com>
2020-03-03 15:00:24 +0100

    Bouton voir plus fonctionnel, il fait apparaîte 100 autres images au hasard

---
+++ asset/javascript/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  // Pour voir 100 autres film
+  $('.button').on('click', function(){
+
+    $('.none').css('display', 'block');
+    $('.button').css('display', 'none');
+
+
+
+  })
+
+
+
+
+
+
+
+
+});
--- inc/listfilm.php
+++ inc/listfilm.php
@@ -8,9 +8,9 @@ $movies = $query->fetchAll();
 ?>
 
 <div class="wrap" id="content">
+    <!-- Ci-dessous la liste des films qui apparaît tout le temps -->
   <?php foreach ($movies as $movie):
     $file = 'posters/'.$movie['id'].'.jpg'; ?>
-
     <div class="filmlist">
       <p><?= $movie['title']; ?></p>
       <?php if (file_exists($file)) { ?>
@@ -20,9 +20,34 @@ $movies = $query->fetchAll();
       <?php } ?>
     </div>
   <?php endforeach; ?>
-    <div class="clear">
 
+
+  <div class="clear"></div>
+  <!-- Bouton pour voir plus -->
+  <div><a class="button" href="#" onclick="return false;">Voir plus</a></div>
+
+<!-- Ci-dessous la liste des films qui apparaît quand on appuye sur voir plus -->
+<?php
+$sql = "SELECT * FROM movies_full ORDER BY RAND() LIMIT 100";
+$query = $pdo->prepare($sql);
+$query->execute();
+$movies = $query->fetchAll();
+
+
+   foreach ($movies as $movie):
+    $file = 'posters/'.$movie['id'].'.jpg'; ?>
+    <div class="filmlist none">
+      <p><?= $movie['title']; ?></p>
+      <?php if (file_exists($file)) { ?>
+        <div><a href="detail.php?slug=<?= $movie['slug'] ?>"><img src="posters/<?= $movie['id'] ?>.jpg" alt="random image"></a></div>
+      <?php } else { ?>
+        <div><a href="detail.php?slug=<?= $movie['slug'] ?>"><img src="posters/empty.jpg" alt="empty image"></a></div>
+      <?php } ?>
     </div>
+  <?php endforeach; ?>
+
+  <div class="clear"></div>
+
 
 
 </div>
