2f63308f2c8c4c34805982f5ceada0666097af62
youngrecursive <baz.martin42@gmail.com>
2020-03-03 12:47:48 +0100

    modif listfilm.php, quand l'image est vide, une image par défaut apparaît

--- inc/listfilm.php
+++ inc/listfilm.php
@@ -2,15 +2,22 @@
 $sql = "SELECT * FROM movies_full ORDER BY RAND() LIMIT 100";
 $query = $pdo->prepare($sql);
 $query->execute();
-$movies = $query->fetchAll(); ?>
+$movies = $query->fetchAll();
 
 
+?>
 
 <div class="wrap" id="content">
-  <?php foreach ($movies as $movie): ?>
+  <?php foreach ($movies as $movie):
+    $file = 'posters/'.$movie['id'].'.jpg'; ?>
+
     <div class="filmlist">
       <p><?= $movie['title']; ?></p>
-      <div><a href="detail.php?slug=<?= $movie['slug'] ?>"><img src="posters/<?= $movie['id'] ?>.jpg" alt="random image"></a></div>
+      <?php if (file_exists($file)) { ?>
+        <div><a href="detail.php?slug=<?= $movie['slug'] ?>"><img src="posters/<?= $movie['id'] ?>.jpg" alt="random image"></a></div>
+      <?php } else { ?>
+        <div><a href="detail.php?slug=<?= $movie['slug'] ?>"><img src="posters/empty.jpg" alt="empty image"></a></div>
+      <?php } ?>
     </div>
   <?php endforeach; ?>
     <div class="clear">
