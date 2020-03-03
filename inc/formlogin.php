5014bed6504ff11fd1d1e8f34d5b11cb248cb227
Philippe Montury <pmontury@yahoo.fr>
2020-03-03 16:22:13 +0100

    modif css

--- inc/formlogin.php
+++ inc/formlogin.php
@@ -1,19 +1,17 @@
-<?php
-   $pageTitle = 'Connection';
-   ?>
    <div class="wrap" id="content">
+   <h1>Connexion</h1>
     <form class="" action="login.php" method="post">
-
-
-     <label for="pseudo">Pseudo</label>
+     <label for="pseudo">Pseudo<span>*</span></label>
      <input type="text" name="login" id="login" value="<?php if(!empty($_POST['login'])){echo $_POST['login'];} ?>">
+     <p class="error"></p>
+
+     <label for="password">Mot de passe<span>*</span></label>
+     <input type="password" name="password" value="<?php if(!empty($_POST['password'])){echo $_POST['password'];} ?>">
+
      <p class="error"><?php if(!empty($errors['login'])) {echo $errors['login'];} ?></p>
 
-     <label for="password">Mot de passe</label>
-     <input type="password" name="password" value="<?php if(!empty($_POST['password'])){echo $_POST['password'];} ?>">
-     
+     <input class="submit" type="submit" name="submitlogin" value="CONNEXION">
+     <br>
      <a class="forgetpass" href="oublie.php">Mot de passe oublié</a>
-     <p class="error"><?php if(!empty($errors['password'])) {echo $errors['password'];} ?></p>
-     <input type="submit" name="submitlogin" value="CONNEXION">
     </form>
   </div>
--- inc/formoublie.php
+++ inc/formoublie.php
@@ -1,13 +1,10 @@
-<?php
-   $pageTitle = 'Mot de passe oublié';
-   ?>
+<div class="wrap" id="content">
    <h1>Mot de passe oublié</h1>
-   <div class="wrap" id="content">
-     <form class="" action="" method="post">
-       <label for="email">E-mail</label>
+   <form class="" action="" method="post">
+      <label for="email">E-mail</label>
       <input type="email" name="email" id="email" value="">
-      <p class="error"><?php if(!empty($errors['email'])) {echo $errors['email'];} ?>
+      <p class="error"><?php if(!empty($errors['email'])) {echo $errors['email'];} ?></p>
 
       <input type="submit" name="submitoublie" value="Réinitialiser votre mot de passe">
-     </form>
-   </div>
+   </form>
+</div>
