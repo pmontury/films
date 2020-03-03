08deeee6a5ec05e87b85e4001599f3969e45c6ed
youngrecursive <baz.martin42@gmail.com>
2020-03-03 15:13:30 +0100

    Le bouton voir plus fait simplement un refresh de page index et ne fait pas apparaître 200 films au total mais bien 100

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
 
-  // Pour voir 100 autres film
-  $('.button').on('click', function(){
 
-    $('.none').css('display', 'block');
-    $('.button').css('display', 'none');
-
-
-
-  })
 
 
 
--- inc/listfilm.php
+++ inc/listfilm.php
@@ -24,27 +24,8 @@ $movies = $query->fetchAll();
 
   <div class="clear"></div>
   <!-- Bouton pour voir plus -->
-  <div><a class="button" href="#" onclick="return false;">Voir plus</a></div>
+  <div><a class="button" href="index.php">Voir plus</a></div>
 
-<!-- Ci-dessous la liste des films qui apparaît quand on appuye sur voir plus -->
-<?php
-$sql = "SELECT * FROM movies_full ORDER BY RAND() LIMIT 100";
-$query = $pdo->prepare($sql);
-$query->execute();
-$movies = $query->fetchAll();
-
-
-   foreach ($movies as $movie):
-    $file = 'posters/'.$movie['id'].'.jpg'; ?>
-    <div class="filmlist none">
-      <p><?= $movie['title']; ?></p>
-      <?php if (file_exists($file)) { ?>
-        <div><a href="detail.php?slug=<?= $movie['slug'] ?>"><img src="posters/<?= $movie['id'] ?>.jpg" alt="random image"></a></div>
-      <?php } else { ?>
-        <div><a href="detail.php?slug=<?= $movie['slug'] ?>"><img src="posters/empty.jpg" alt="empty image"></a></div>
-      <?php } ?>
-    </div>
-  <?php endforeach; ?>
 
   <div class="clear"></div>
 
