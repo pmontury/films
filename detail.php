de164a4d2929d92c24d51f357a95bd96954d01ee
AliciaCODE <alicia.leger@hotmail.fr>
2020-03-04 10:52:44 +0100

    detail note

--- detail.php
+++ detail.php
@@ -1,6 +1,8 @@
 <?php
    require('inc/func.php');
    $errors = array();
+   $success = false;
+   //------------------- details des films ----------------------------//
    if(!empty($_GET['slug'])) {
     $slug = $_GET['slug'];
     $sql = "SELECT * FROM movies_full WHERE slug = :slug";
@@ -12,11 +14,39 @@
   } else {
     die('404');
   }
+  // ---------------------ajouter une note au films --------------------------//
+  if(!empty($_POST['submitnote'])) {
+    $notation = trim(strip_tags($_POST['notation']));
+    $errors = validText($errors,$notation,'notation',0,100);
+
+    if(count($errors) == 0) {
+      $success = true;
+      $sql = "INSERT INTO t_notes VALUES (NULL, :user_id, :movie_id,:note,NOW(),NOW())";
+      $query = $pdo->prepare($sql);
+      $query->bindvalue(':user_id',$user_id,PDO::PARAM_INT);
+      $query->bindValue(':movie_id',$movie_id, PDO::PARAM_STR);
+      $query->bindValue(':note',$note, PDO::PARAM_STR);
+      $query->execute();
+      header('Location: list_note.php');
+    } else {
+         die('404');
+      }
+  }
+
+
+
+
+
+
+
 
    include('inc/html.php');
    include('inc/header.php'); ?>
+
+
     <div class="wrap" id="content">
        <div class="film">
+         <!--//////////// details des films ////////////////-->
           <h2><?= $film['slug']; ?></h2>
           <img src="posters/<?= $film['id'] .'.jpg' ?>" alt="<?= $film['title']; ?>">
           <h2><?= $film['directors']; ?></h2>
@@ -25,7 +55,18 @@
           <p>Modifié le <?php echo date('d/m/Y',strtotime($film['modified'])) ?></p>
        </div>
        <?php    if (isLogged()) { ?>
+       <!--//////////// ajouter aux films à voirs ///////////////-->
        <a href="films_a_voir.php?id=<?= $film['id']; ?>">A voir</a>
+
+       <!--/////////// donner une note aux films  /////////////-->
+       <form action="detail.php" method="post">
+         <label for="notation">Notes</label>
+         <div class="clear"></div>
+         <input type="number" id="notation" name="notation" value="">
+         <p class="error"><?php if(!empty($errors['notation'])) {echo $errors['notation'];} ?></p>
+
+         <input type="submit" name="submitnote" value="Envoyer">
+       </form>
        <?php  } ?>
     </div>
 
