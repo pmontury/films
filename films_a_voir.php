816652c2dd9c4964d8d53760398684eeed462da6
AliciaCODE <alicia.leger@hotmail.fr>
2020-03-04 09:24:51 +0100

    film a voir fonc

--- films_a_voir.php
+++ films_a_voir.php
@@ -7,7 +7,6 @@ require('inc/func.php');
     $query = $pdo->prepare($sql);
     $query->bindvalue(':user_id',$user_id,PDO::PARAM_INT);
     $query->bindValue(':movie_id',$movie_id, PDO::PARAM_STR);
-    $query->bindValue(':modified_at', $modified_at, PDO::PARAM_STR);
     $query->execute();
   }
   header('Location: list_note.php');
--- list_note.php
+++ list_note.php
@@ -2,6 +2,17 @@
    require('inc/func.php');
    $pageTitle = 'Bonjour';
    $errors = array();
+   if (isLogged()) {
+     $movie_id = $_GET['id'];
+     $user_id = $_SESSION['user']['id'];
+     $sql = "SELECT * FROM t_notes WHERE id = :id";
+     $query = $pdo->prepare($sql);
+     $query->bindvalue(':user_id',$user_id,PDO::PARAM_INT);
+     $query->bindValue(':movie_id',$movie_id, PDO::PARAM_STR);
+     $query->bindValue(':modified_at', $modified_at, PDO::PARAM_STR);
+     $query->execute();
+   }
+   header('Location: list_note.php');
 
    include('inc/html.php');
    include('inc/header.php');
