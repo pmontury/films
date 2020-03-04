21b35a89248908d8d5bdfbaafec67b0f8a22bc0d
youngrecursive <baz.martin42@gmail.com>
2020-03-04 15:18:18 +0100

    Possibilité de filtrer selon date et genre du film sur la liste des films

--- inc/listfilm.php
+++ inc/listfilm.php
@@ -3,83 +3,95 @@
 
   <?php
   if (!empty($_POST['submitted'])){
-      debug($_POST);
+      // Déclaration générale sql
+      $sql = "SELECT * FROM movies_full WHERE 1=1";
+      // If année renseignée
+      if (!empty($_POST['annee'])) {
+        $annee = trim(strip_tags($_POST['annee']));
+        $an = explode('-',$annee);
+        $sql .= " AND year BETWEEN :firstyear AND :lastyear";
+      } // Fin du if catégorie renseignée
+      if (!empty($_POST['cat'])){
+        $categories = $_POST['cat'];
+        $i = 1;
+        debug($categories);
+        foreach ($categories as $categorie) {
+          if($i == 1){
+            $sql .= " AND genres LIKE :categorie".$i;
+          } else {
+            $sql .= " OR genres LIKE :categorie".$i;
+          }
+          $i++;
+        }
 
-      $annee = $_POST['annee'];
-      $an = explode('-',$annee);
-      debug($an);
+      }
+      // Fin de la déclaration générale
+      $sql .= " ORDER BY RAND() LIMIT 100";
 
-      $sql = "SELECT * FROM movies_full WHERE :firstyear <= year <= :lastyear LIMIT 100";
+      // On tape enfin dans le SQL
       $query = $pdo->prepare($sql);
-      $query->bindValue(':firstyear',$an['0'],PDO::PARAM_STR);
-      $query->bindValue(':lastyear',$an['1'],PDO::PARAM_STR);
-      $query->execute();
-      $movies = $query->fetchAll(); ?>
-
-      <?php foreach ($movies as $movie):
-        $file = 'posters/'.$movie['id'].'.jpg'; ?>
-        <div class="filmlist">
-          <p><?= $movie['title']; ?></p>
-          <p><?= $movie['year']; ?></p>
-          <?php if (file_exists($file)) { ?>
-            <div><a href="detail.php?slug=<?= $movie['slug'] ?>"><img src="posters/<?= $movie['id'] ?>.jpg" alt="random image"></a></div>
-          <?php } else { ?>
-            <div><a href="detail.php?slug=<?= $movie['slug'] ?>"><img src="posters/empty.jpg" alt="empty image"></a></div>
-          <?php } ?>
-        </div>
-      <?php endforeach; ?>
 
 
-  <?php } ?>
+      if (!empty($_POST['annee'])){
+        $query->bindValue(':firstyear',$an['0'],PDO::PARAM_STR);
+        $query->bindValue(':lastyear',$an['1'],PDO::PARAM_STR);
+      }
+      if (!empty($_POST['cat'])){
+        $i = 1;
+        foreach ($categories as $categorie) {
+          $cate = trim(strip_tags($categorie));
+          $query->bindValue(':categorie'.$i,'%'.$cate.'%',PDO::PARAM_STR);
+          $i++;
+        }
+      }
 
 
+        $query->execute();
+        $movies = $query->fetchAll();
+        //debug($movies);
+}  else {
+  $sql = "SELECT * FROM movies_full ORDER BY RAND() LIMIT 100";
+    $query = $pdo->prepare($sql);
+        $query->execute();
+  $movies =  $query->fetchAll();
+} ?>
 
 
+<div class="wrap">
 
-  <div class="wrap" id="content">
     <form class="" action="" method="post">
-
       <select name="annee">
+        <option value="">no</option>
         <option value="1920-1930">1920 1930</option>
         <option value="1930-1940">1930 1940</option>
       </select>
 
-      <input type="checkbox" name="cat[]" value="Drama">Drama
-      <input type="checkbox" name="cat[]" value="Action">Action
+      <?php $types = array('Drama', 'Action', 'Adventure', 'Horror', 'Sci-Fi');
+      foreach ($types as $type) { ?>
+      <input type="checkbox" name="cat[]" value="<?= $type ?>" <?php if(!empty($_POST['submitted']) AND !empty($categories) AND in_array($type,$categories)) { ?> checked="checked" <?php } ?>><?= $type ?>
+      <!-- <input type="checkbox" name="cat[]" value="Action">Action
       <input type="checkbox" name="cat[]" value="Adventure">Adventure
+      <input type="checkbox" name="cat[]" value="Horror">Horror
+      <input type="checkbox" name="cat[]" value="Sci-Fi">Sci-Fi -->
+    <?php } ?>
 
       <input type="submit" name="submitted" value="recherche">
 
     </form>
 
-  <!-- On va cherche dans le base de données -->
-  <?php
-  $sql = "SELECT * FROM movies_full ORDER BY RAND() LIMIT 100";
-  $query = $pdo->prepare($sql);
-  $query->execute();
-  $movies = $query->fetchAll(); ?>
-
-  <!-- Ci-dessous la liste des films qui apparaît tout le temps -->
-  <?php foreach ($movies as $movie):
-    $file = 'posters/'.$movie['id'].'.jpg'; ?>
-    <div class="filmlist">
-      <p><?= $movie['title']; ?></p>
-      <?php if (file_exists($file)) { ?>
-        <div><a href="detail.php?slug=<?= $movie['slug'] ?>"><img src="posters/<?= $movie['id'] ?>.jpg" alt="random image"></a></div>
-      <?php } else { ?>
-        <div><a href="detail.php?slug=<?= $movie['slug'] ?>"><img src="posters/empty.jpg" alt="empty image"></a></div>
-      <?php } ?>
-    </div>
-  <?php endforeach; ?>
-
-
+<?php
+        foreach ($movies as $movie):
+          $file = 'posters/'.$movie['id'].'.jpg'; ?>
+          <div class="filmlist">
+            <p><?= $movie['title']; ?></p>
+            <p><?= $movie['year']; ?></p>
+            <p><?= $movie['genres']; ?></p>
+            <?php if (file_exists($file)) { ?>
+              <div><a href="detail.php?slug=<?= $movie['slug'] ?>"><img src="posters/<?= $movie['id'] ?>.jpg" alt="random image"></a></div>
+            <?php } else { ?>
+              <div><a href="detail.php?slug=<?= $movie['slug'] ?>"><img src="posters/empty.jpg" alt="empty image"></a></div>
+            <?php } ?>
+          </div>
+        <?php endforeach; ?>
   <div class="clear"></div>
-  <!-- Bouton pour voir plus -->
-  <div><a class="button" href="index.php">Voir plus</a></div>
-
-
-  <div class="clear"></div>
-
-
-
 </div>
