f2611aa62aaa358cd979551454921b21fb74ae87
AliciaCODE <alicia.leger@hotmail.fr>
2020-03-04 09:35:12 +0100

    debut list_note

--- inc/header.php
+++ inc/header.php
@@ -17,7 +17,7 @@
                <li><a href="admin/index.php">Administration</a></li>
 <?php       } ?>
             <li><a href="deconnexion.php">Déconnexion</a></li>
-            <li><a href="films_a_voir.php">A voir</a></li>
+            <li><a href="list_note.php">A voir</a></li>
             <li class="bonjour">Bonjour <?= $_SESSION['user']['pseudo']; ?></li>
 <?php    } ?>
          </ul>
--- list_note.php
+++ list_note.php
@@ -3,16 +3,12 @@
    $pageTitle = 'Bonjour';
    $errors = array();
    if (isLogged()) {
-     $movie_id = $_GET['id'];
      $user_id = $_SESSION['user']['id'];
-     $sql = "SELECT * FROM t_notes WHERE id = :id";
+     $sql = "SELECT * FROM t_notes WHERE user_id = :user_id";
      $query = $pdo->prepare($sql);
      $query->bindvalue(':user_id',$user_id,PDO::PARAM_INT);
-     $query->bindValue(':movie_id',$movie_id, PDO::PARAM_STR);
-     $query->bindValue(':modified_at', $modified_at, PDO::PARAM_STR);
      $query->execute();
    }
-   header('Location: list_note.php');
 
    include('inc/html.php');
    include('inc/header.php');
